90e28b0a260613db8039ca2c4b019cca33c4a031
Lauge Møller Andersen <46719124+LaugeM@users.noreply.github.com>
2026-02-26 22:46:05 +0100

    Added repositories for models

---
+++ Budweg/Models/EmployeeRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Budweg.Models
+{
+    public class EmployeeRepository : RepoBase<Employee>
+    {
+        public EmployeeRepository() : base()
+        {
+        }
+    }
+}
