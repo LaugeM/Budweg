e756050c2f8e0a964bfc633d6881650701ae7cb4
Marcus will <marcuswill5974@gmail.com>
2026-03-03 11:24:37 +0100

    Made some basic UI, and started on Commands

---
+++ Budweg/Commands/AddCaliperCommand.cs
@@ -0,0 +1,43 @@
+﻿using Budweg.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace Budweg.Commands
+{
+    public class AddCaliperCommand : ICommand
+    {
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            bool result = true;
+
+            if (parameter is RegCaliperViewModel rvm)
+            {
+                if (rvm.SelectedIncome != null && rvm.CheckSelectedIncomeInRepo() == false)
+                {
+                    result = false;
+                }
+            }
+            return result;
+        }
+
+        public void Execute(object parameter)
+        {
+            if (parameter is RegCaliperViewModel rvm)
+            {
+                rvm.AddCaliper();
+            }
+            else
+            {
+                throw new ArgumentException("Illegal parameter type");
+            }
+        }
+    }
+}
