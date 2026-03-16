9524ac797541c1d569392b481e5a5b8a4c018ff3
Lauge Møller Andersen <46719124+LaugeM@users.noreply.github.com>
2026-03-16 12:09:25 +0100

    Fully implemented adding caliper, with both new and pre-existing stampnumber.

--- Budweg/Models/CaliperRepository.cs
+++ Budweg/Models/CaliperRepository.cs
@@ -40,25 +40,57 @@ namespace Budweg.Models
 
                 if (string.IsNullOrEmpty(caliper.StampNumber))
                 {
+                    // Brand new caliper, generate stamp and insert
                     using SqlCommand cmd = new SqlCommand("dbo.GenerateStampNumber", con);
                     cmd.CommandType = CommandType.StoredProcedure;
-                    cmd.Parameters.AddWithValue("@Manufacturer", caliper.Manufacturer);
-                    cmd.Parameters.AddWithValue("@Approval", caliper.Approval);
-                    cmd.Parameters.AddWithValue("@ModelNumber", caliper.ModelNumber);
+                    cmd.Parameters.Add("@Manufacturer", System.Data.SqlDbType.NVarChar, 50).Value = caliper.Manufacturer;
+                    cmd.Parameters.Add("@Approval", System.Data.SqlDbType.Bit).Value = caliper.Approval;
+                    cmd.Parameters.Add("@ModelNumber", System.Data.SqlDbType.NVarChar, 20).Value = caliper.ModelNumber;
                     caliper.StampNumber = cmd.ExecuteScalar().ToString().Trim();
                 }
                 else
                 {
-                    using SqlCommand insert = new SqlCommand(@"
-                    INSERT INTO dbo.CALIPER (StampNumber, Manufacturer, Approval, ModelNumber)
-                    VALUES (@StampNumber, @Manufacturer, @Approval, @ModelNumber)", con);
+                    // Stamp number provided, check if it already exists
+                    using SqlCommand checkCmd = new SqlCommand(@"
+                    SELECT COUNT(*) FROM dbo.CALIPER 
+                    WHERE StampNumber = @StampNumber", con);
+                    checkCmd.Parameters.Add("@StampNumber", System.Data.SqlDbType.Char, 8).Value = caliper.StampNumber;
+                    bool exists = (int)checkCmd.ExecuteScalar() > 0;
 
-                    insert.Parameters.AddWithValue("@StampNumber", caliper.StampNumber);
-                    insert.Parameters.AddWithValue("@Manufacturer", caliper.Manufacturer);
-                    insert.Parameters.AddWithValue("@Approval", caliper.Approval);
-                    insert.Parameters.AddWithValue("@ModelNumber", caliper.ModelNumber);
-                    insert.ExecuteNonQuery();
+                    if (exists)
+                    {
+                        // Returning caliper, increment counter
+                        using SqlCommand renovateCmd = new SqlCommand(@"
+                        UPDATE dbo.CALIPER
+                        SET TimesRenovated = TimesRenovated + 1, Approval = @Approval
+                        WHERE StampNumber = @StampNumber;", con);
+                        renovateCmd.Parameters.Add("@StampNumber", System.Data.SqlDbType.Char, 8).Value = caliper.StampNumber;
+                        renovateCmd.Parameters.Add("@Approval", System.Data.SqlDbType.Bit).Value = caliper.Approval;
+                        renovateCmd.ExecuteNonQuery();
+                    }
+                    else
+                    {
+                        // New caliper with pre-existing stamp, insert into CALIPER
+                        using SqlCommand insertCmd = new SqlCommand(@"
+                        INSERT INTO dbo.CALIPER (StampNumber, Manufacturer, Approval, ModelNumber)
+                        VALUES (@StampNumber, @Manufacturer, @Approval, @ModelNumber)", con);
+                        insertCmd.Parameters.Add("@StampNumber", System.Data.SqlDbType.Char, 8).Value = caliper.StampNumber;
+                        insertCmd.Parameters.Add("@Manufacturer", System.Data.SqlDbType.NVarChar, 50).Value = caliper.Manufacturer;
+                        insertCmd.Parameters.Add("@Approval", System.Data.SqlDbType.Bit).Value = caliper.Approval;
+                        insertCmd.Parameters.Add("@ModelNumber", System.Data.SqlDbType.NVarChar, 20).Value = caliper.ModelNumber;
+                        insertCmd.ExecuteNonQuery();
+                    }
                 }
+
+                // Always insert a renovation record, regardless of which branch we took
+                using SqlCommand renovationCmd = new SqlCommand(@"
+                INSERT INTO dbo.RENOVATION (StampNumber, RegistrationDate)
+                VALUES (@StampNumber, @RegistrationDate);", con);
+                renovationCmd.Parameters.Add("@StampNumber", System.Data.SqlDbType.Char, 8).Value = caliper.StampNumber;
+                renovationCmd.Parameters.Add("@RegistrationDate", System.Data.SqlDbType.Date).Value = DateTime.Today;
+                renovationCmd.ExecuteNonQuery();
+
+                caliper.StampNumber = null;
             }
         }
     }
